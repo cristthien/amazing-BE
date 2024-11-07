5503409c18f61c05fd57e35bd019bf638c749da4
cristthien <cristhuuthien@gmail.com>
2024-11-08 00:19:50 +0700

    config docker

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,11 +3,11 @@ import { User } from '../user/entities/user.entity';
 
 export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [User],
   synchronize: true,
 });
